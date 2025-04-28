cc3934268df4b62b78139c0a8215d71ac15c683b
BishalParajulii <parajulibishal00@gmail.com>
2025-04-28 12:00:34 +0545

    added repo

--- events/serializers.py
+++ events/serializers.py
@@ -5,15 +5,19 @@ from .models import Event , Participant
 
 
 class EventSerializer(serializers.ModelSerializer):
+    organizer = serializers.SerializerMethodField()
     class Meta:
         model = Event
-        fields = ['name', 'venue', 'location', 'time', 'description', 'link_or_qr', 'created_by']
+        fields = ['name', 'venue', 'location', 'time', 'description', 'link_or_qr', 'organizer']
 
     def create(self, validated_data):
         # Set the 'created_by' field to the current authenticated user
         user = self.context['request'].user  # Access 'request' from the context
         validated_data['created_by'] = user
         return super().create(validated_data)
+
+    def get_organizer(self , obj):
+        return obj.created_by.username
 
 
 class ParticipantSerializer(serializers.ModelSerializer):
